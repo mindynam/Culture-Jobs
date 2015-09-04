d63d9be34048bf63e713a70b1959d0138da3bbe5
mindynam <mindygnam@gmail.com>
2015-09-04 12:14:52 -0400

    linked to angular

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
 
 //        functions!
 var cursorChange = function (element) {
-    $(element).mouseover(function(){    
+    $(document).on("mouseover", element, function(){    
         $(this).css('cursor','pointer');
 });
 }
-cursorChange('.star');
 
 
 $(document).ready(function(){
+    cursorChange('#menu-icon');
+    cursorChange('.star');
     var i = 0;
     var j = 0;
     var t = 500;
-    $('.see-more').click(function(){    
-        if (i%2 ===0){
-            var dHeight = $('.description').height();
-            var mtop = $('.job-post').css('margin-top');
-            $(this).parent().animate({height:dHeight+80},t);
-            $('.description-wrapper').animate({height:dHeight+10},t);
-            $('.see-more').html('<span class="down-triangle" style="color:#acacac">▲</span>  SEE LESS  <span class="down-triangle" style="color:#acacac">▲</span>');
-            $('.see-more').css('color','#acacac');
-            $(this).parent().parent().next().animate({'marginTop':dHeight-40},t);
-        }
-        else {
 
-            $(this).parent().animate({height:'143px'},t);                            
-            $('.description-wrapper').animate({height:'73px'},t);
-            $('.see-more').html('<span class="down-triangle">▼</span>  SEE MORE  <span class="down-triangle">▼</span>');
-            $('.see-more').css('color','#DF550A');
+    $(document).on("click", '.see-more', function(){    
+        if ($(this).hasClass('see-less')){
+
+            $(this).parent().animate({height:'145px'},t);                            
+            $(this).parent().children('.description-wrapper').animate({height:'103px'},t);
+            $(this).html('<span class="down-triangle">▼</span>  SEE MORE  <span class="down-triangle">▼</span>');
+            $(this).css('color','#DF550A');
+            $(this).removeClass('see-less');
             $(this).parent().parent().next().animate({'marginTop':20},t);
         }
-        i++;
+        else{
+            var dHeight = $('.description').height();
+            var mtop = $('.job-post').css('margin-top');
+            $(this).parent().animate({height:dHeight+140},t);
+            $(this).parent().children('.description-wrapper').animate({height:dHeight+80},t);
+            $(this).html('<span class="down-triangle" style="color:#acacac">▲</span>  SEE LESS  <span class="down-triangle" style="color:#acacac">▲</span>');
+            $(this).css('color','#acacac');
+            $(this).addClass('see-less');
+            $(this).parent().parent().next().animate({'marginTop':dHeight+28},t);
+        }
+    
         });
-    $('.star').click(function(){
+    $(document).on("click", '.star', function(){
+        
         if (j%2 ==0){
             $(this).css('color','#DF550A');
         }
