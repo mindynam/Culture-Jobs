8888971bc866e3691a881e0dc3f1366b80f771b4
mindynam <mindygnam@gmail.com>
2015-09-06 07:25:11 -0400

    Added velocity.js
    - Also added more animation (scrolling paper, automatic reorientation,
    fixed background)
    - Need to figure out stacking order

--- main.js
+++ main.js
@@ -6,36 +6,55 @@ var cursorChange = function (element) {
 });
 }
 
+var showOverlay = function(x){
+    $(".overlay").insertAfter($(this).parent().parent());
+    $(".overlay").show();
+    $(x).parent().parent().css("z-index","2");
+        }
+    
+var hideOverlay = function(x){
+    $(".overlay").insertAfter($(this).parent().parent());
+    $(".overlay").hide();
+    $(x).parent().parent().css("z-index","1");
+        }    
+
 
 $(document).ready(function(){
     cursorChange('#menu-icon');
     cursorChange('.star');
-    var i = 0;
     var j = 0;
     var t = 500;
 
-    $(document).on("click", '.see-more', function(){    
+    $(document).on("click", '.see-more', function(e){  
         if ($(this).hasClass('see-less')){
-
-            $(this).parent().animate({height:'145px'},t);                            
+            $('body').css('overflow','auto');
+            $(this).parent().velocity({height:'145px'},t);                            
             $(this).parent().children('.description-wrapper').animate({height:'103px'},t);
             $(this).html('<span class="down-triangle">▼</span>  SEE MORE  <span class="down-triangle">▼</span>');
             $(this).css('color','#DF550A');
             $(this).removeClass('see-less');
-            $(this).parent().parent().next().animate({'marginTop':20},t);
+            hideOverlay($(this));
+//            $(this).parent().parent().next().animate({'marginTop':20},t);
         }
         else{
+            e.preventDefault();
+            $('body').css('overflow','hidden');
+            $(this).velocity("scroll", {offset: -300, duration: 500, easing: "ease" });
             var dHeight = $('.description').height();
             var mtop = $('.job-post').css('margin-top');
-            $(this).parent().animate({height:dHeight+135},t);
+//            $(this).parent().velocity({height:dHeight+135},t);
+            $(this).parent().velocity({height:'500px',t});
             $(this).parent().children('.description-wrapper').animate({height:dHeight+80},t);
+            
+            $(this).parent().css("overflow","scroll");
             $(this).html('<span class="down-triangle" style="color:#acacac">▲</span>  SEE LESS  <span class="down-triangle" style="color:#acacac">▲</span>');
             $(this).css('color','#acacac');
             $(this).addClass('see-less');
-            $(this).parent().parent().next().animate({'marginTop':dHeight+28},t);
+            showOverlay($(this));
         }
-    
-        });
+    });
+        
+        
     $(document).on("click", '.star', function(){
         
         if (j%2 ==0){
